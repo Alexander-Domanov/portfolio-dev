4c4820a4b15b0b3806b4b480e56fcf8713fac3d1
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2026-08-10 16:16:48 +0300

    refactor: extract skills module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,9 @@ import '../../scss/main.scss';
 import { initAbout } from './about';
 import { initHeader } from './header';
 import { initHero } from './hero';
+import { initSkills } from './skills';
 
 initHeader();
 initHero();
 initAbout();
+initSkills();
