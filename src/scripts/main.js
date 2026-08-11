9e14cf408c67fd551585734bbe67744518f42e56
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2026-08-11 13:43:48 +0300

    feat: redesign header and hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,9 @@ import '../../scss/main.scss';
 import { initAbout } from './about';
 import { initFooter } from './footer';
 import { initHeader } from './header';
-import { initHero } from './hero';
 import { initSkills } from './skills';
 
 initHeader();
-initHero();
 initAbout();
 initSkills();
 initFooter();
