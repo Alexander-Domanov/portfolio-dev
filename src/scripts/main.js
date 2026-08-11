e65aed34b4d7b7715b6c518eb4f5f675e2cd0d0a
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2026-08-11 23:02:46 +0300

    feat: redesign skills section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,7 @@ import '../../scss/main.scss';
 import { initAbout } from './about';
 import { initFooter } from './footer';
 import { initHeader } from './header';
-import { initSkills } from './skills';
 
 initHeader();
 initAbout();
-initSkills();
 initFooter();
